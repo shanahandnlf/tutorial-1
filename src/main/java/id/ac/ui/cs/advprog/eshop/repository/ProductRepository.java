1e7a8ee2c96db2c5ea5c3a168fc7de15b480ebfd
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-02-07 01:14:59 +0700

    fix typo route in product controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -10,10 +10,12 @@ import java.util.List;
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+
     public Product create(Product product) {
-        productData.add (product);
+        productData.add(product);
         return product;
     }
+
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -1,13 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.service;
+
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
 @Service
 public class ProductServiceImpl implements ProductService {
 
