5f0f38ca51674f4ceabbf244d1d38d3c9414663e
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-02-23 18:19:52 +0700

    edit sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,15 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
-    id("org.sonarqube") version "3.5.0.2730"
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "shanahandnlf_tutorial-1")
+        property("sonar.organization", "shanahandnlf")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 group = "id.ac.ui.cs.advprog"
