4621ca04b6912b46ed041d42e128547ab9488caf
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-03-06 06:35:50 +0700

    [RED] Add OrderRepository skeleton

---
+++ src/main/java/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package repository;
+
+import model.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class OrderRepository {
+    private List<Order> orderData = new ArrayList<>();
+
+    public Order save(Order order) {
+        return null;
+    }
+
+    public Order findById(String id) {
+        return null;
+    }
+
+    public List<Order> findAllByAuthor(String author) {
+        return null;
+    }
+}
