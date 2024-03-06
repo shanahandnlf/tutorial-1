0bb4b9e3a7539538e16b1741ebb2b8cce32fc7e2
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-03-06 19:48:47 +0700

    [GREEN] implement PaymentServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 
@@ -7,8 +9,8 @@ import java.util.List;
 import java.util.Map;
 
 public interface PaymentService {
-    Payment addPayment(Order order, String method, Map<String, String> paymentData);
-    void setStatus(Payment payment, String status);
+    Payment addPayment(Order order, PaymentMethod method, Map<String, String> paymentData);
+    void setStatus(Payment payment, PaymentStatus status);
     Payment getPayment(String id);
     List<Payment> getAllPayment();
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
@@ -9,29 +12,45 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
-    @Autowired
-    private PaymentRepository paymentRepository;
 
-    @Override
-    public Payment addPayment(Order order, String method, Map<String, String> paymentData) { \
-        return null;
+    private final PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
     }
 
     @Override
-    public void setStatus(Payment payment, String status) {
+    public Payment addPayment(Order order, PaymentMethod method, Map<String, String> paymentData) {
+        if (paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data cannot be empty");
+        }
+        Payment payment = new Payment(UUID.randomUUID().toString(), method, paymentData, order);
+        return paymentRepository.save(payment);
+    }
 
+    @Override
+    public void setStatus(Payment payment, PaymentStatus status) {
+        payment.setStatus(status);
+
+        if (payment.getStatus() == PaymentStatus.SUCCESS) {
+            payment.getOrder().setStatus(OrderStatus.SUCCESS.getValue());
+        } else if (payment.getStatus() == PaymentStatus.REJECTED) {
+            payment.getOrder().setStatus(OrderStatus.FAILED.getValue());
+        }
     }
 
     @Override
     public Payment getPayment(String id) {
-        return null;
+        return paymentRepository.findById(id);
     }
 
     @Override
     public List<Payment> getAllPayment() {
-        return null;
+        return paymentRepository.getAllPayments();
     }
 }
